495fd3ce59499c8180e7b902e1ba499a2d702723
Priyanka-Sodhi <priyankasodhi00@gmail.com>
2020-08-07 20:02:47 +0530

    3 files modified, 3 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, SearchBarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
